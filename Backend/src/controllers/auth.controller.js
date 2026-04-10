2fa1b2296229f7fe52afd656eb79456c52fa101c
Nileshnhe <nehenilesh111@gmail.com>
2026-04-10 13:05:37 +0530

    fix: set cookie with httpOnly and secure options

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken"
 
 // Token Generate
 const generateToken = (id, role) => {
-    return jwt.sign({id, role}, process.env.JWT_SECRET, {
+    return jwt.sign({ id, role }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN,
     });
 };
@@ -21,9 +21,9 @@ export const register = async (req, res) => {
         const { name, email, password, role } = req.body;
 
 
-        const existingAdmin = await adminModel.findOne({email});
-        if(existingAdmin) {
-            return res.status(400).json({message: "Admin already exists with this email"})
+        const existingAdmin = await adminModel.findOne({ email });
+        if (existingAdmin) {
+            return res.status(400).json({ message: "Admin already exists with this email" })
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -36,21 +36,24 @@ export const register = async (req, res) => {
         });
 
         const token = generateToken(admin._id, admin.role);
-        res.cookie("token", token)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+        })
 
         res.status(201).json({
             success: true,
             token,
-            admin:{
+            admin: {
                 id: admin._id,
                 name: admin.name,
                 email: admin.email,
-                role: admin.role,    
+                role: admin.role,
             },
         });
 
     } catch (error) {
-          res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
 };
 
