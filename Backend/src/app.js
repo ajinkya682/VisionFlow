b8a2e68a281d0576ca5226da2e9a90a1797f3632
Nileshnhe <nehenilesh111@gmail.com>
2026-04-10 11:22:16 +0530

    feat: enhance security and performance with helmet, cors, rate limiting, and body parsing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,32 @@
 import express from "express"
+import cors from "cors"
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
 
 const app = express();
- 
 
+// security
+app.use(helmet());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+
+
+// Rate Limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests, please try again later."
+})
+app.use("/api", limiter);
+
+// Body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Health checker
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "Server is running" })
 })
